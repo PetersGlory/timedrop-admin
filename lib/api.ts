f30488e5afca849ad67df0068b8c6e6937925cfc
Peter Thomas <peterglo2200@gmail.com>
2026-02-18 15:02:40 +0100

    clean-up

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:450/api'//'https://backendapi.timedrop.live/api'; //'https://timedrop-backend.onrender.com/api';
+const BASE_URL = 'https://backendapi.timedrop.live/api'; //'https://timedrop-backend.onrender.com/api';
 
 // --- Types (adjust as needed based on backend models) ---
 export interface User {
